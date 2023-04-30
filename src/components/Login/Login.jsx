1d15cc7ed6d8cae0531e88ce6a02c9771bae534f
Bajlar01~ <bajlarrahman24@gmail.com>
2023-04-30 09:57:16 +0600

    first commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import './Login.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault();
+        
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form onSubmit={handleLogin}>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+                <p className='text-center'><small>New to Ema-john? <Link to='/signup' className='text-orange'>Create New Account</Link></small></p>
+            </form>
         </div>
     );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVk0w3pqU5ssNLC4Fl5Ma0JVbEwkkRw1Y",
+  authDomain: "ema-john-with-firebase-a-10589.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-10589",
+  storageBucket: "ema-john-with-firebase-a-10589.appspot.com",
+  messagingSenderId: "925833716564",
+  appId: "1:925833716564:web:22fce05e0191712ed89ec1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
